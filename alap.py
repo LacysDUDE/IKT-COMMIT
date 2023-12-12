39f0dde74cabea4d677f5ba20e14a000e104044d
LacysDUDE <harschlaci@gmail.com>
2023-12-12 08:40:47 +0100

    Almost done

--- alap.py
+++ alap.py
@@ -1,6 +1,6 @@
 #BMI kalkulátor
 
-suly = int(input("Hány kg vagy?: "))
+suly = float(input("Hány kg vagy?: "))
 magassag = float(input("Ird be a magasságot méterben!: "))
 
 szamolas = suly / magassag**2
@@ -12,7 +12,6 @@ print(magassag, "méter magas vagy")
 print(szamolas)
 
 print("Táblázat")
-print("Táblázat")
-print("Táblázat")
-print("Táblázat")
-print("Táblázat")
+print("Vékony | <0.68 - 0.74")
+print("Normál | 0.74 - 1")
+print("Túlsúly | 1 - 1.2")
