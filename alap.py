e625cbfebc47df1919119c6c2514d4c189c74e39
LacysDUDE <harschlaci@gmail.com>
2023-12-12 08:28:37 +0100

    forward

--- alap.py
+++ alap.py
@@ -1,10 +1,12 @@
 #BMI kalkulátor
 
 suly = int(input("Hány kg vagy?: "))
+magassag =int(input("Ird be a magasságot centibe!: "))
 
-
-
+szamolas =
 
 
 
 print(suly," kg vagy.")
+print(magassag, "cm magas vagy")
+print()
